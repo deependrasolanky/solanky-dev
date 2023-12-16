a0bc18485b951ac185f97b88551695cb91acddd5
Deependra Solanky <solanky@gmail.com>
2023-12-16 11:45:59 +0530

    Fetching Model

--- src/routes/browser-test/+page.svelte
+++ src/routes/browser-test/+page.svelte
@@ -1,7 +1,10 @@
 <script>
     let agent = navigator.userAgent;
     console.log(agent);
-    let agentClientHints = navigator.userAgentData;
+    let agentClientHints = "";
+    navigator.userAgentData
+.getHighEntropyValues(['architecture', 'bitness', 'brands', 'mobile', 'model', 'platform', 'platformVersion', 'uaFullVersion', 'fullVersionList', 'wow64'])
+.then(ua => { agentClientHints = ua });
     console.log(agentClientHints);
 </script>
 <p>{agent}</p>
