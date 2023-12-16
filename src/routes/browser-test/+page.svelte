9bba599146077b23943d2529252a72db8dad8db8
Deependra Solanky <solanky@gmail.com>
2023-12-16 12:07:15 +0530

    Tailwind CSS

--- src/routes/browser-test/+page.svelte
+++ src/routes/browser-test/+page.svelte
@@ -7,5 +7,10 @@
 .then(ua => { agentClientHints = ua });
     console.log(agentClientHints);
 </script>
-<p>{agent}</p>
-<p>{JSON.stringify(agentClientHints)}</p>
+<h1 class="text-3xl font-bold">Brand and Model : {agentClientHints.model ? agentClientHints.model :"Not Found!"}</h1>
+<br>
+<h2 class="text-2xl font-bold">Other Details</h2>
+<h3 class="text-xl font-bold">Old Method:</h3>
+<div class="inline-block">{agent}</div>
+<h3 class="text-xl font-bold">New Method:</h3>
+<div class="inline-block">{JSON.stringify(agentClientHints)}</div>
